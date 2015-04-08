2bd9e9f4e8f88fd2ec220b6453ed49a1d5e691c4
oscarotero <oom@oscarotero.com>
2015-04-08 18:17:51 +0200

    improvements, tests, fixes

--- tests/bench.js
+++ tests/bench.js
@@ -4,14 +4,18 @@ var suite = require('benchmark').Suite();
 var v1 = require('../node_modules/stylecow-parser/lib'); 
 var v2 = require('../lib');
 
-var code = fs.readFileSync(__dirname + '/css/1.css', 'utf-8');
+var large = fs.readFileSync(__dirname + '/bench/large.css', 'utf-8');
+var small = fs.readFileSync(__dirname + '/bench/small.css', 'utf-8');
 
 suite
 	.add('v1', function () {
-		v1.Root.create(new v1.Reader(code));
+		v1.Root.create(new v1.Reader(small));
 	})
 	.add('v2', function () {
-		v2.Root.create(v2.Reader.fromString(code));
+		v2.Root.create(v2.Reader.fromString(small));
+	})
+	.add('v2-large', function () {
+		v2.Root.create(v2.Reader.fromString(large));
 	})
 	.on('cycle', function(event) {
 		console.log(String(event.target));

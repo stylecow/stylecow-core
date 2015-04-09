5ecaefb277b77c79eb565a537febc4916654889a
oscarotero <oom@oscarotero.com>
2015-04-09 11:13:39 +0200

    removed duplicated classes

--- tests/tests.js
+++ tests/tests.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var stylecow = require('../lib');
 
-//var css = stylecow.Root.create(stylecow.Reader.fromFile(path.join(__dirname, 'bench', 'plus.google.com.css')));
+//var css = stylecow.Root.create(stylecow.Reader.fromFile(path.join(__dirname, 'bench', 'github.com.css')));
 //process.exit();
 
 var cases = fs.readdirSync(path.join(__dirname, 'bench'));

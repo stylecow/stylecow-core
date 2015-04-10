4b70ccdad1fecc7ed394d807c70fb319da9b21c6
oscarotero <oom@oscarotero.com>
2015-04-10 20:49:07 +0200

    advances

--- tests/tests.js
+++ tests/tests.js
@@ -2,8 +2,13 @@ var fs = require('fs');
 var path = require('path');
 var stylecow = require('../lib');
 
-//var css = stylecow.Root.create(stylecow.Reader.fromFile(path.join(__dirname, 'bench', 'github.com.css')));
-//process.exit();
+var css = stylecow.Root.create(stylecow.Reader.fromFile(path.join(__dirname, 'bench', 'github.com.css')));
+
+var coder = new stylecow.Coder(css, {
+});
+
+console.log(coder.code);
+process.exit();
 
 var cases = fs.readdirSync(path.join(__dirname, 'bench'));
 

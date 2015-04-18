34993916e0cda19f14b2930af4523170766e78b4
oscarotero <oom@oscarotero.com>
2015-04-18 18:35:21 +0200

    more tests

--- tests/cases.js
+++ tests/cases.js
@@ -1,7 +1,19 @@
 var stylecow = require('../lib');
 
 stylecow.testCases(__dirname + '/cases', function (name, css, assert) {
+	//assert.regenerate();
+
 	describe('cases/' + name, function() {
+		it('should match output.css and ast.json', function() {
+			assert();
+		});
+	});
+});
+
+stylecow.testCases(__dirname + '/tests', function (name, css, assert) {
+	//assert.regenerate();
+
+	describe('tests/' + name, function() {
 		it('should match output.css and ast.json', function() {
 			assert();
 		});

c4d44e03f5bbed5be9d6c0254c56bce083870167
oscarotero <oom@oscarotero.com>
2016-09-21 16:37:20 +0200

    do not print empty rules

--- tests/cases.js
+++ tests/cases.js
@@ -2,13 +2,20 @@ var assert   = require('assert');
 var stylecow = require('../lib');
 
 var cases = new stylecow.Test(__dirname + '/cases');
+var coder = new stylecow.Coder();
 
 cases.cssErrors(true).run(function (test) {
     describe('cases/' + test.name, function() {
-
         it('should match output.css', function() {
             //test.writeString()
             test.assertString();
+        });
+
+        var code = coder.run(test.css);
+
+        it('should match output.normal.css', function() {
+            //test.write('output.normal.css', code.css);
+            assert.equal(test.normalize(code.css), test.read('output.normal.css'));
         });
 
         it('clone should match output.css', function() {
